4cfbebd6353cb74caf0c4d41676bd5b25fd33776
parkjihun22 <eyeful31@naver.com>
2025-03-06 11:35:09 +0900

    feat : 주소변경경

--- src/UrlList.js
+++ src/UrlList.js
@@ -17,7 +17,7 @@ const UrlList = [
     { image: img1, name: "브레인시티 푸르지오", url: "https://www.bunyang-114.com", image_alt: "브레인시티푸르지오" },
     { image: img3, name: "푸르지오 원클러스터 2단지", url: "https://www.beyinegzersizi.com", image_alt: "용인푸르지오원클러스터2단지" },
     { image: img5, name: "대전도안 벽산블루밍", url: "https://www.abcya4.com", image_alt: "도안벽산블루밍" },
-    { image: img6, name: "대전 하늘채 스카이앤 3차", url: "https://www.kikaporn.com", image_alt: "대전하늘채스카이앤3차" },
+    { image: img6, name: "대전 하늘채 스카이앤 3차", url: "https://buysildenafilv.com", image_alt: "대전하늘채스카이앤3차" },
     { image: img2, name: "평택 브레인시티 수자인", url: "https://www.ys-sujain.co.kr", image_alt: "평택브레인시티수자인" },
     { image: img7, name: "엘리프 세종 5-1", url: "https://www.sekailog.com", image_alt: "엘리프세종5-1" },
     { image: img8, name: "강동역 센트럴파크", url: "https://www.donnyien.com", image_alt: "강동역센트럴파크" },
